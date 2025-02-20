70b4ba2dcd5c65800ee02eedeb27f8d877ea7c5b
Cbermont <89049830+bermont13@users.noreply.github.com>
2025-02-20 03:41:38 +0000

    landing page

---
+++ src/js/components/Card.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const Card = () => {
+  return (
+    
+      
+        <div className='col'>
+          <div class="card" style={{ width: "18rem " }}>
+            <img src="https://desarrollowebyseo.com.mx/wp-content/uploads/2022/10/Diseno-de-la-web-1024x683.jpg" class="card-img-top" alt="Diseño web personalizado" />
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <div class="d-flex justify-content-center align-items-center">
+
+              <a href="#" class="btn btn-primary justify-content-center gap">Go somewhere</a>
+              </div>
+            </div>
+            </div>
+          </div>
+        
+      
+        
+  )
+}
+
+export default Card;
